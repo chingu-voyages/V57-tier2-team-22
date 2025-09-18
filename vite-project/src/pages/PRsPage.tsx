29c6a1d6fe7de21065ec4903a58faf346b5acd8a
Jazz <106460341+jazxbx@users.noreply.github.com>
2025-09-18 01:21:06 -0700

    create Tabs component and integrate it into PRsPage

---
+++ vite-project/src/pages/PRsPage.tsx
@@ -0,0 +1,9 @@
+import Tabs from '../components/Tabs';
+
+export default function PRsPage() {
+  return (
+    <main className='bg-bg-main h-screen p-3 md:p8 lg:px-14 lg:py-11 flex flex-col'>
+      <Tabs />
+    </main>
+  );
+}
